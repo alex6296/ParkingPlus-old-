049dcb131ed52cb8038bc989ecfcc801a66e2e96
Alex <32260519+alex6296@users.noreply.github.com>
2020-03-11 18:45:55 +0100

    user permission request

--- app/src/main/java/com/example/parkingplus/MapsActivity.java
+++ app/src/main/java/com/example/parkingplus/MapsActivity.java
@@ -1,10 +1,15 @@
 package com.example.parkingplus;
 
+import android.Manifest;
 import android.app.Dialog;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -20,14 +25,23 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private static final String TAG = "MainActivity";
+
+    private int userPermissionRequestCode = 1;
     private GoogleMap mMap;
+    private Boolean userPermission = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
         //Checking if google services is working
         if(googleServicesWorks()){
 
-            super.onCreate(savedInstanceState);
+            while (!userPermission){ // nags the user about giving location permission
+                getLocationUserPermission();
+            }
+
             setContentView(R.layout.activity_maps);
 
             // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -37,6 +51,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         }
     }
+
+    private void getLocationUserPermission(){
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            Log.d(TAG, "getLocationUserPermission: user has given permission ");
+            this.userPermission = true;
+        }
+        Log.d(TAG, "getLocationUserPermission: requesting user permission ");
+        String[] userPermissions = {Manifest.permission.ACCESS_FINE_LOCATION};
+        ActivityCompat.requestPermissions(this, userPermissions,userPermissionRequestCode);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == userPermissionRequestCode){ // matches our request send to get permission
+               for(int i = 0; i < grantResults.length; i++){ // loops all request grands
+                   if(grantResults[i] != PackageManager.PERMISSION_GRANTED){ // checks if permission was given
+                       // if 1 permission was denied set permission to false and stop
+                       userPermission=false;
+                       break;
+                   }
+                   else{ // if all permission was granted set userPermission to true
+                       userPermission=true;
+                   }
+               }
+            }
+        }
+
 
     private boolean googleServicesWorks(){
         Log.d(TAG, "googleServiceVersionCheck: checking validity service version ");
