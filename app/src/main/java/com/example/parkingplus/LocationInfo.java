04df2f805a42d1c2fe3a9527e7c65833869c0a2d
Sebastian <Sebastiangildenpfennig@gmail.com>
2020-05-16 14:44:13 +0200

    Listview for location
    Fixed a list view and created appropriate adapters

--- app/src/main/java/com/example/parkingplus/LocationInfo.java
+++ app/src/main/java/com/example/parkingplus/LocationInfo.java
@@ -6,21 +6,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class LocationInfo extends Fragment {
     private Button goBackBtn;
+    private RecyclerView locationsList;
     private Button test;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private String[] locationDataSet;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.locationinf, container, false);
 
         goBackBtn = (Button) view.findViewById(R.id.goBackBtn);
+        locationsList = (RecyclerView) view.findViewById(R.id.locationsList);
+        locationsList.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(view.getContext());
+        locationsList.setLayoutManager(layoutManager);
+
+        mAdapter = new LocationAdapter(locationDataSet);
+        locationsList.setAdapter(mAdapter);
 
 
 
